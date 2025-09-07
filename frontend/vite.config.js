c80c364cf46a81910f70502ddb0a270fc73d3605
Sandy Bimo H <sandybimo.2023@student.uny.ac.id>
2025-09-07 20:10:03 +0700

    Fix .gitignore to ignore build and cache folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Kampiun/', // ⚡ ini penting biar GitHub Pages ngerti path-nya
   plugins: [react()],
 })
